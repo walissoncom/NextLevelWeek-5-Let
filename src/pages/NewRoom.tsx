3401a1953ebc1928ca51bbd7a8bc05012e3fa669
Walisson Rodrigues <contact@walisson.com>
2021-06-26 23:24:02 +1000

    NewRoom screen

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,31 @@
+import IllustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+export function NewRoom() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={IllustrationImg} alt="Illustration representing questions and answers" />
+                <strong>Create live rooms of Q&amp;A</strong>
+                <p>Answer questions from your audience in real time</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <h2>Create a new room</h2>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Room name"
+                        />
+                        <Button type="submit">Create room</Button>
+                    </form>
+                    <p>Entering an existing room? <a href="">Click here</a></p>
+                </div>
+            </main>
+        </div>
+    )
+}

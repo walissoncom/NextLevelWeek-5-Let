64af4200ad3b63692ccb506f27f0db0ac36e41ad
Walisson Rodrigues <contact@walisson.com>
2021-06-26 23:15:15 +1000

    Authentication screen

---
+++ src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import IllustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+import googleIconImg from '../assets/images/google-icon.svg'
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+//webpack (snowpack, vite, ...) handles imports
+// Module Bundle -> webpack (gets the file extension) -> Render the JSX
+
+export function Home() {
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={IllustrationImg} alt="Illustration representing questions and answers" />
+                <strong>Create live rooms of Q&amp;A</strong>
+                <p>Answer questions from your audience in real time</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <button className="create-room">
+                        <img src={googleIconImg} alt="Google's logo" />
+                        Create your room with Google
+                    </button>
+                    <div className="separator">or enter a room</div>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder="Type the code's room"
+                        />
+                        <Button type="submit">Enter room</Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    )
+}
